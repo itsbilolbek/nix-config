b11b7d1e353ea478fc7ed98c4086cbda3fea0894
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2025-12-17 15:56:33 +0900

    More zed settings

--- editors/zed.nix
+++ editors/zed.nix
@@ -3,15 +3,23 @@
     enable = true;
 
     userSettings = {
+      auto_update = false;
       disable_ai = true;
       telemetry.metrics = false;
       telemetry.diagnostics = false;
       collaboration_panel.dock = "right";
+      collaboration_panel.button = false;
       git_panel.dock = "right";
       outline_panel.dock = "right";
       project_panel.dock = "right";
       buffer_line_height = "standard";
-      autosave.after_delay.milliseconds = 1000;
+      autosave = "on_focus_change";
+      formate_on_save = "on";
+      tabs = {
+        file_icons = true;
+        git_status = true;
+        show_diagnostics = "errors";
+      };
       base_keymap = "VSCode";
       show_whitespaces = "boundary";
       minimap.show = "auto";
