a786fa7fb2d79ba466df8084f6a0c92c17b95145
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2025-12-19 15:52:12 +0900

    Reorganize home.nix

--- home.nix
+++ home.nix
@@ -3,11 +3,12 @@
   pkgs,
   inputs,
   ...
-}: {
-  imports = [
+}: let
+  modules = [
     ./alacritty.nix
     ./firefox.nix
     ./stylix.nix
+    ./packages.nix
     ./cmd/bat.nix
     ./cmd/delta.nix
     ./cmd/fzf.nix
@@ -19,111 +20,48 @@
     ./editors/helix.nix
     ./editors/zed.nix
   ];
+in {
+  imports = modules;
 
-  home.username = "kebol";
-  home.homeDirectory = "/home/kebol";
+  programs.home-manager.enable = true;
 
-  home.stateVersion = "25.11";
-
-  nix.package = pkgs.nix;
-
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    substituters = [
-      "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  home = {
+    username = "kebol";
+    homeDirectory = "/home/kebol";
+    stateVersion = "25.11";
+    sessionVariables = {
+      EDITOR = "hx";
+      VISUAL = "hx";
+      TERMINAL = "alacritty";
+      XDG_TERMINAL_EMULATOR = "alacritty";
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
-    # Language formatters
-    alejandra
-    black
-    prettier
-    shfmt
-
-    # LSP
-    bash-language-server
-    clang-tools
-    nixd # Nix language server
-    pyright
-    typescript-language-server
-
-    # Linters
-    shellcheck # required for bash-language-server
-    eslint
-
-    # Debuggers
-    lldb
-
-    # GUI
-    alacritty
-    telegram-desktop
-    zed-editor
-
-    # Archiving
-    zip
-    unzip
-
-    bash
-    bat
-    btop
-    coreutils
-    curl
-    delta
-    direnv
-    dust
-    eza
-    fastfetch
-    fontconfig
-    fd
-    fzf
-    gcc
-    gdb
-    git
-    gnupg
-    helix
-    hunspell
-    jq
-    moreutils # sponge util
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.hack
-    ripgrep
-    sd
-    starship
-    sqlite
-    tealdeer
-    tree-sitter
-    wget
-    yazi
-    zellij
-    zoom-us
-    zsh
-    zoxide
-  ];
-
-  nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"]; # Use system nixpkgs for nixd lsp
-
-  home.sessionVariables = {
-    EDITOR = "hx";
-    VISUAL = "hx";
-    TERMINAL = "alacritty";
-    XDG_TERMINAL_EMULATOR = "alacritty";
+  nix = {
+    package = pkgs.nix;
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"]; # Use system nixpkgs for nixd lsp
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   xdg.enable = true;
 
   fonts.fontconfig.enable = true;
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  targets.genericLinux.enable = true;
-  targets.genericLinux.gpu.enable = true;
+  # Enable graphics on non-NixOS distros
+  targets.genericLinux = {
+    enable = true;
+    gpu.enable = true;
+  };
 }
---
+++ packages.nix
@@ -0,0 +1,67 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # Language formatters
+    alejandra
+    black
+    prettier
+    shfmt
+
+    # LSP
+    bash-language-server
+    clang-tools
+    eslint
+    nixd # Nix language server
+    pyright
+    shellcheck # required for bash-language-server
+    typescript-language-server
+
+    # Development
+    lldb
+    gcc
+    gdb
+    git
+    sqlite
+    tree-sitter
+
+    # GUI
+    alacritty
+    telegram-desktop
+    zed-editor
+    zoom-us
+
+    # Archiving
+    zip
+    unzip
+
+    # Terminal and text utils
+    bat
+    btop
+    coreutils
+    curl
+    delta
+    direnv
+    dust
+    eza
+    fd
+    fzf
+    jq
+    moreutils # sponge util
+    ripgrep
+    sd
+    tealdeer
+    wget
+    yazi
+    zellij
+    zoxide
+
+    fastfetch
+    fontconfig
+    gnupg
+    helix
+    hunspell
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.hack
+    starship
+    zsh
+  ];
+}
