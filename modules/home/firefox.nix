7fca2c7f34e13c16d491ca7d3d94546579334209
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2025-12-30 10:31:59 +0500

    Firefox no title bar

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -6,7 +6,6 @@
       settings = {
         "layout.css.always_underline_links" = true;
         "sidebar.verticalTabs" = true;
-        "browser.tabs.inTitlebar" = 0;
       };
 
       extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
