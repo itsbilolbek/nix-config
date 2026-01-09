5a4e48429996c931d0365cbbead53935ea13053a
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-09 17:00:29 +0500

    Add korean fonts

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -116,6 +116,21 @@
     mangohud
   ];
 
+  fonts.packages = with pkgs; [
+    nanum # Classic Korean fonts (Gothic, Myeongjo, Pen, Brush)
+    noto-fonts-cjk-sans # Google's high-quality CJK fonts
+    noto-fonts-cjk-serif
+    baekmuk-ttf # Older but standard compatibility fonts
+  ];
+
+  fonts.fontconfig = {
+    defaultFonts = {
+      serif = ["Noto Serif CJK KR" "NanumMyeongjo"];
+      sansSerif = ["Noto Sans CJK KR" "NanumGothic"];
+      monospace = ["Noto Sans Mono CJK KR" "NanumGothicCoding"];
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
