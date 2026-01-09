49d27648e7819fc646652cc347563a3a9b5bafdb
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-09 10:25:41 +0500

    Hide firefox title bar and bookmarks toolbar. Add leechblock-ng add-on

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -6,11 +6,14 @@
       settings = {
         "layout.css.always_underline_links" = true;
         "sidebar.verticalTabs" = true;
+        "browser.tabs.inTitlebar" = 1;
+        "browser.toolbars.bookmarks.visibility" = "never";
       };
 
       extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
         bitwarden
         ublock-origin
+        leechblock-ng
       ];
     };
   };
