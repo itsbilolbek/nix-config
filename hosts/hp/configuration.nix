195ab278fc846d82f04ed6851a5e06c8e5269d8c
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-20 10:43:31 +0500

    Reorganize configuration.nix

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -1,41 +1,45 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../modules/nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  system.stateVersion = "25.11";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Asia/Tashkent";
 
-  # Select internationalisation properties.
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "uzx,us";
+    options = "ctrl:nocaps";
+    extraLayouts.uzx = {
+      description = "Uzbek (US)";
+      languages = ["uzb"];
+      symbolsFile = ./uz;
+    };
+  };
+
   i18n = {
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
       "ko_KR.UTF-8/UTF-8"
     ];
+
     inputMethod = {
       enable = true;
       type = "fcitx5";
@@ -49,6 +53,23 @@
     };
   };
 
+  fonts = {
+    fontconfig = {
+      defaultFonts = {
+        serif = ["Noto Serif CJK KR" "NanumMyeongjo"];
+        sansSerif = ["Noto Sans CJK KR" "NanumGothic"];
+        monospace = ["Noto Sans Mono CJK KR" "NanumGothicCoding"];
+      };
+    };
+
+    packages = with pkgs; [
+      nanum # Classic Korean fonts (Gothic, Myeongjo, Pen, Brush)
+      noto-fonts-cjk-sans # Google's high-quality CJK fonts
+      noto-fonts-cjk-serif
+      baekmuk-ttf # Older but standard compatibility fonts
+    ];
+  };
+
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver = {
@@ -57,47 +78,29 @@
     desktopManager.cinnamon.enable = true;
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "uzx,us";
-    options = "ctrl:nocaps";
-    extraLayouts.uzx = {
-      description = "Uzbek (US)";
-      languages = ["uzb"];
-      symbolsFile = ./uz;
+  services = {
+    printing.enable = true;
+    pulseaudio.enable = false;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    libinput = {
+      enable = true;
+      touchpad.naturalScrolling = true;
+      mouse = {
+        # Disable mouse acceleration
+        accelProfile = "flat";
+        accelSpeed = "0";
+      };
     };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput = {
-    enable = true;
-    mouse = {
-      # This disables the "smart" acceleration that often causes jitters
-      accelProfile = "flat";
-      # Optional: adjust sensitivity if it feels too slow after flattening
-      accelSpeed = "0";
-    };
-    touchpad.naturalScrolling = true;
-  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mocha = {
@@ -105,92 +108,24 @@
     description = "Bilolbek";
     extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.zsh;
-    # packages = with pkgs; [];
   };
 
-  # Install firefox.
-  #   programs.firefox.enable = true;
-
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
     mangohud
   ];
 
-  fonts.packages = with pkgs; [
-    nanum # Classic Korean fonts (Gothic, Myeongjo, Pen, Brush)
-    noto-fonts-cjk-sans # Google's high-quality CJK fonts
-    noto-fonts-cjk-serif
-    baekmuk-ttf # Older but standard compatibility fonts
-  ];
-
-  fonts.fontconfig = {
-    defaultFonts = {
-      serif = ["Noto Serif CJK KR" "NanumMyeongjo"];
-      sansSerif = ["Noto Sans CJK KR" "NanumGothic"];
-      monospace = ["Noto Sans Mono CJK KR" "NanumGothicCoding"];
-    };
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-  nix.settings = {
-    experimental-features = ["nix-command" "flakes"];
-    auto-optimise-store = true;
-  };
-
-  programs.zsh.enable = true;
-  # programs.ssh.startAgent = true;
-
-  # Gaming settings
-  hardware.graphics = {
-    enable = true;
-    # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  };
-
   services.xserver.videoDrivers = ["amdgpu"];
 
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-  };
+  hardware.graphics.enable = true;
 
-  programs.gamemode.enable = true;
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-    persistent = true;
+  programs = {
+    zsh.enable = true;
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
   };
 }
--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -12,7 +12,8 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 14d";
+      persistent = true;
     };
 
     settings = {
