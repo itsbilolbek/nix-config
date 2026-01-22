10068a2275d2cc35e04a48eceacc10fd70625cc9
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-22 11:05:23 +0500

    Move virtualisation settings to a module

--- hosts/xenon/configuration.nix
+++ hosts/xenon/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ./hardware-configuration.nix
     ./gaming
+    ./virtualisation
     ../../modules/nix
   ];
 
@@ -128,26 +129,6 @@
       "podman"
     ];
     shell = pkgs.zsh;
-  };
-
-  environment.systemPackages = [ pkgs.podman-compose ];
-
-  virtualisation = {
-    containers = {
-      registries.search = [
-        "docker.io"
-        "quay.io"
-        "ghcr.io"
-      ];
-      storage.settings = {
-        storage.driver = "overlay";
-      };
-    };
-    podman = {
-      enable = true;
-      dockerSocket.enable = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
   };
 
   programs.zsh.enable = true;
---
+++ hosts/xenon/virtualisation/default.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = [ pkgs.podman-compose ];
+
+  virtualisation = {
+    containers = {
+      registries.search = [
+        "docker.io"
+        "quay.io"
+        "ghcr.io"
+      ];
+      storage.settings = {
+        storage.driver = "overlay";
+      };
+    };
+    podman = {
+      enable = true;
+      dockerSocket.enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+}
