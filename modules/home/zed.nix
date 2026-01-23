bcbe9204f228ac7c222d4cdc27d8837148598e26
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-23 12:19:26 +0500

    Add language servers and formatters for zed. Enable path lookup

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -14,8 +14,10 @@
       auto_update = false;
       cursor_blink = false;
       disable_ai = false;
-      telemetry.metrics = false;
-      telemetry.diagnostics = false;
+      telemetry = {
+        metrics = false;
+        diagnostics = false;
+      };
       collaboration_panel.button = false;
       buffer_line_height = "standard";
       autosave = "on_focus_change";
@@ -23,7 +25,7 @@
       icon_theme = "Colored Zed Icons Theme Dark";
       buffer_font_family = "JetBrainsMono Nerd Font Mono";
       ui_font_family = "JetBrainsMono Nerd Font";
-      formate_on_save = "on";
+      format_on_save = "on";
       tabs = {
         file_icons = true;
         git_status = true;
@@ -32,23 +34,43 @@
       base_keymap = "VSCode";
       show_whitespaces = "boundary";
       minimap.show = "never";
+      tab_size = 2;
+      load_direnv = "shell_hook";
+      inlay_hints = {
+        enable = true;
+        show_background = true;
+      };
 
       languages = {
+        C = {
+          language_servers = [ "clangd" ];
+        };
+
+        "C++" = {
+          language_servers = [ "clangd" ];
+        };
+
         Nix = {
           language_servers = [
             "nixd"
             "!nil"
           ];
-          formatter = {
-            external = {
-              command = "alejandra";
-              arguments = [
-                "--quiet"
-                "-"
-              ];
-            };
-          };
+          formatter.external.command = "nixfmt";
         };
+
+        Python = {
+          language_servers = [
+            "basedpyright"
+            "ruff"
+          ];
+          formatter.language_server.name = "ruff";
+        };
+      };
+      lsp = {
+        basedpyright.binary.path_lookup = true;
+        clangd.binary.path_lookup = true;
+        nixd.binary.path_lookup = true;
+        ruff.binary.path_lookup = true;
       };
     };
   };
