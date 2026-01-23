3678d20199273d946724502e57bc8a48ee1dc91b
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-23 22:08:41 +0500

    Git hooks follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -19,7 +19,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
