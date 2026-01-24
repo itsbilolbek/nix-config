82610b8fbec7ceb15066459629f85fa5c1a76101
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-24 23:27:46 +0500

    Add gh options

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -5,62 +5,72 @@
     delta
   ];
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
+  programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
 
-    ignores = [
-      "*.swp"
-      "*~"
-      "*result"
-    ];
+      ignores = [
+        "*.swp"
+        "*~"
+        "*result"
+      ];
 
-    settings = {
-      user = {
-        name = "Normuminov Bilolbek";
-        email = "bilolbeknormuminov@gmail.com";
-        signingkey = "~/.ssh/id_ed25519.pub";
+      settings = {
+        user = {
+          name = "Normuminov Bilolbek";
+          email = "bilolbeknormuminov@gmail.com";
+          signingkey = "~/.ssh/id_ed25519.pub";
+        };
+
+        init.defaultBranch = "main";
+        pull.rebase = true;
+        rebase.autoStash = true;
+        gpg.format = "ssh";
+
+        core = {
+          editor = "${config.home.sessionVariables.EDITOR}";
+          autocrlf = "input";
+        };
+
+        alias = {
+          aa = "add -A";
+          br = "branch";
+          ci = "commit";
+          co = "checkout";
+          st = "status -s";
+          dog = "log --oneline --graph --decorate --all";
+          unstage = "reset HEAD --";
+        };
       };
 
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      rebase.autoStash = true;
-      gpg.format = "ssh";
-
-      core = {
-        editor = "${config.home.sessionVariables.EDITOR}";
-        autocrlf = "input";
-      };
-
-      alias = {
-        aa = "add -A";
-        br = "branch";
-        ci = "commit";
-        co = "checkout";
-        st = "status -s";
-        dog = "log --oneline --graph --decorate --all";
-        unstage = "reset HEAD --";
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/id_ed25519"; # # Leave blank when using the ssh format above
+        signByDefault = true;
+        format = "ssh";
       };
     };
 
-    signing = {
-      key = "${config.home.homeDirectory}/.ssh/id_ed25519"; # # Leave blank when using the ssh format above
-      signByDefault = true;
-      format = "ssh";
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+
+      options = {
+        side-by-side = true;
+        line-numbers = true;
+        dark = true;
+        syntax-theme = "gruvbox-dark";
+        conflictStyle = "zdiff3";
+        navigate = true;
+      };
     };
-  };
 
-  programs.delta = {
-    enable = true;
-    enableGitIntegration = true;
-
-    options = {
-      side-by-side = true;
-      line-numbers = true;
-      dark = true;
-      syntax-theme = "gruvbox-dark";
-      conflictStyle = "zdiff3";
-      navigate = true;
+    gh = {
+      enable = true;
+      settings = {
+        git_protocol = "ssh";
+        prompt = "enabled";
+      };
     };
   };
 }
