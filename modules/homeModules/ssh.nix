b6c420fa47247249fc5ec5e7a6986e0be56a951b
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-02-04 15:56:45 +0500

    Let home manager manage ssh keys

--- modules/homeModules/ssh.nix
+++ modules/homeModules/ssh.nix
@@ -1,44 +1,41 @@
 { inputs, ... }:
-let
-  sshPath = "/home/mocha/.ssh/id_ed25519";
-in
 {
-  flake.nixosModules.sops =
+  flake.homeModules.ssh =
     { config, ... }:
+    let
+      sshPath = "${config.home.homeDirectory}/.ssh/id_ed25519";
+    in
     {
-      sops.secrets = {
-        "private-keys/mocha" = {
+      imports = [
+        inputs.sops-nix.homeManagerModules.sops
+      ];
+
+      sops = {
+        age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+        defaultSopsFile = "${inputs.my-secrets}/secrets.yaml";
+        secrets."private-keys/mocha" = {
           path = sshPath;
-          owner = config.users.users.mocha.name;
-          inherit (config.users.users.mocha) group;
-          mode = "0600";
         };
       };
-    };
 
-  flake.homeModules.ssh = {
-    imports = [
-      inputs.sops-nix.homeManagerModules.sops
-    ];
+      home.file.".ssh/id_ed25519.pub".text =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEoLiI2EBrig34UzX2xY22+FK5JeYfUO5IqmyahjJSQ bilolbeknormuminov@gmail.com";
 
-    home.file.".ssh/id_ed25519.pub".text =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEoLiI2EBrig34UzX2xY22+FK5JeYfUO5IqmyahjJSQ bilolbeknormuminov@gmail.com";
+      programs.ssh = {
+        enable = true;
+        enableDefaultConfig = false;
 
-    programs.ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-
-      matchBlocks = {
-        "*" = { };
-        "github.com" = {
-          hostname = "github.com";
-          user = "git";
-          identityFile = sshPath;
-          addKeysToAgent = "yes";
+        matchBlocks = {
+          "*" = { };
+          "github.com" = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = sshPath;
+            addKeysToAgent = "yes";
+          };
         };
       };
-    };
 
-    services.ssh-agent.enable = true;
-  };
+      services.ssh-agent.enable = true;
+    };
 }
