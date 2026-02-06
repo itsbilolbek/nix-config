a53902c03df9f5856d2804f26b0d1143eef40c62
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-02-06 15:17:57 +0500

    feat: terminal copy and paste

--- modules/homeModules/fish.nix
+++ modules/homeModules/fish.nix
@@ -2,7 +2,13 @@
   flake.homeModules.fish =
     { pkgs, ... }:
     {
-      home.packages = [ pkgs.fish ];
+      home.packages = with pkgs; [
+        fish
+
+        # copy and paste in terminal
+        wl-clipboard
+        xclip
+      ];
 
       programs.fish = {
         enable = true;
@@ -26,6 +32,23 @@
 
         functions = {
           template.body = "nix flake init -t github:itsbilolbek/flake-templates#\"$argv\"";
+          cb.body = ''
+            if isatty stdin
+              # Paste logic: Try wayland first, then X11
+              if set -q WAYLAND_DISPLAY
+                wl-paste
+              else
+                xclip -selection clipboard -o
+              end
+            else
+              # Copy logic: Read from stdin and send to clipboard
+              if set -q WAYLAND_DISPLAY
+                wl-copy
+              else
+                xclip -selection clipboard
+              end
+            end
+          '';
         };
       };
     };
