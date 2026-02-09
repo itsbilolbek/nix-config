d298e0b1216834761051a6599e5b79ebf2485d91
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2026-02-09 17:48:14 +0500

    chore: wrap helix

--- modules/nixosModules/core/general.nix
+++ modules/nixosModules/core/general.nix
@@ -1,19 +1,13 @@
 {
-  flake.nixosModules.core =
-    { pkgs, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        helix
-      ];
+  flake.nixosModules.core = {
+    time.timeZone = "Asia/Tashkent";
 
-      time.timeZone = "Asia/Tashkent";
+    services.xserver.xkb.options = "ctrl:nocaps";
 
-      services.xserver.xkb.options = "ctrl:nocaps";
-
-      i18n = {
-        defaultLocale = "en_US.UTF-8";
-        extraLocales = [ "en_US.UTF-8/UTF-8" ];
-        supportedLocales = [ "en_US.UTF-8/UTF-8" ];
-      };
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocales = [ "en_US.UTF-8/UTF-8" ];
+      supportedLocales = [ "en_US.UTF-8/UTF-8" ];
     };
+  };
 }
