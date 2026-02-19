60c8ad764ce46a948091de9f1265fbbc327f321a
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2026-02-19 14:01:28 +0500

    chore: kitty: new tab with cwd, switch to tab with alt+#

--- modules/homeModules/kitty.nix
+++ modules/homeModules/kitty.nix
@@ -31,6 +31,19 @@
           italic_font = "JetBrainsMonoNFM-SemiBoldItalic";
           bold_italic_font = "JetBrainsMonoNFM-BoldItalic";
         };
+
+        keybindings = {
+          "alt+1" = "goto_tab 1";
+          "alt+2" = "goto_tab 2";
+          "alt+3" = "goto_tab 3";
+          "alt+4" = "goto_tab 4";
+          "alt+5" = "goto_tab 5";
+          "alt+6" = "goto_tab 6";
+          "alt+7" = "goto_tab 7";
+          "alt+8" = "goto_tab 8";
+          "alt+9" = "goto_tab 9";
+          "ctrl+shift+t" = "new_tab_with_cwd";
+        };
       };
     };
 }
