a759f33c7be0236869b64f4adf1dfe1e502b75fd
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2026-04-03 18:12:21 +0900

    chore: revert language to English

--- modules/nixosModules/core/general.nix
+++ modules/nixosModules/core/general.nix
@@ -5,7 +5,7 @@
     services.xserver.xkb.options = "ctrl:nocaps";
 
     i18n = {
-      defaultLocale = "uz_UZ.UTF-8";
+      defaultLocale = "en_US.UTF-8/UTF-8";
       extraLocales = [
         "uz_UZ.UTF-8/UTF-8"
         "en_US.UTF-8/UTF-8"
